c21bedfc398a68562b0c40497960550af3927272
salvatorebriga <salvo.z2302@gmail.com>
2024-07-18 15:47:09 +0200

    added font awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/7711c3f1fc.js" crossorigin="anonymous"></script>
 
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
