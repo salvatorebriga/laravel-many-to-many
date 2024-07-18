9c7aa98c3a9e49b835ecee233ea786c2b7ec5ae9
salvatorebriga <salvo.z2302@gmail.com>
2024-07-18 16:26:14 +0200

    fixed edit view

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -4,6 +4,16 @@
   <div class="container">
     <h1 class="py-4">Edit Project</h1>
 
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
     <form action="{{ route('admin.projects.update', $project->id) }}" method="POST">
       @csrf
       @method('PATCH')
