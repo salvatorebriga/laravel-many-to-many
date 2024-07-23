c0b250071482687819b6b7f25e281ce6e69eb7f9
salvatorebriga <salvo.z2302@gmail.com>
2024-07-23 16:05:02 +0200

    Seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = ['Full Stack', 'Front End', 'Back End', 'Compiled', 'Assembly'];
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+    }
+}
