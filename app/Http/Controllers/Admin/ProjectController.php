310a8a798838bcae6e6ec86cd8dbcff16f2c6c24
salvatorebriga <salvo.z2302@gmail.com>
2024-07-23 16:39:11 +0200

    join

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::with('category')->get();
         return view('admin.projects.index', compact('projects'));
     }
 
