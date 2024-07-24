673b0390c5225dbd25b1e2da09ff3c246593892f
salvatorebriga <salvo.z2302@gmail.com>
2024-07-24 22:53:11 +0200

    added authenticated nav-link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,9 @@
                 <li class="nav-item">
                   <a class="nav-link" href="{{ url('/admin/categories') }}">{{ __('Categories') }}</a>
                 </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('/admin/technologies') }}">{{ __('Technologies') }}</a>
+                </li>
               @endauth
             </ul>
 
