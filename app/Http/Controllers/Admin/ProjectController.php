f15bd49c65912e0df9020640fe0ca85533338324
salvatorebriga <salvo.z2302@gmail.com>
2024-07-24 22:57:53 +0200

    fix

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -56,6 +56,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
+        $project->load('category', 'technologies');
         return view('admin.projects.show', compact('project'));
     }
 
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,7 +5,15 @@
     <h1 class="py-4">{{ $project->name }}</h1>
 
     <p><strong>Description:</strong> {{ $project->description }} </p>
-    <p><strong>Categories:</strong> {{ $project->category->name ?? 'N/A' }} </p>
+    <p><strong>Category:</strong> {{ $project->category->name ?? 'N/A' }} </p>
+
+    <p><strong>Technologies:</strong>
+      @if ($project->technologies->isEmpty())
+        N/A
+      @else
+        {{ $project->technologies->pluck('name')->implode(', ') }}
+      @endif
+    </p>
 
     <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary">Back to Projects</a>
   </div>
